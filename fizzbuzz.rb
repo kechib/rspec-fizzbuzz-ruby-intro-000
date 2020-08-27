8c86a4683c15a49d59e76d9f8099cca633415fab
Kechi Boniface <bonifacekechi@gmail.com>
2020-08-27 18:31:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,17 @@
 def fizzbuzz(number)
-if number % 3 && 5
+if number / 3 && 5
   return "FizzBuzz"
 end
  end
 
 def fizz(number)
-  if number is evenly % 3
+  if number is evenly / 3
     return "Fizz"
   end
 end
 
 def buzz(number)
-  if number is evenly % 5
+  if number is evenly / 5
     return "Buzz"
   end
 end
