859f38b4566b314f0f3e8f9abf5b23529f3a53c3
Kechi Boniface <bonifacekechi@gmail.com>
2020-08-27 18:05:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,16 +3,3 @@ if number % 3 && 5
   return "FizzBuzz"
 end
  end
-
-def fizz(number)
-  if number % 3
-    return "Fizz"
-  end
-end
-
-def buzz(number)
-  if number % 5
-    return "Buzz"
-  else return "nil"
-  end
-end
