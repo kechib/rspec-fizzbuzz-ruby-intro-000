b5a8a99bef73b491edd195c6b80e5b88aee7c68c
Kechi Boniface <bonifacekechi@gmail.com>
2020-08-27 17:21:45 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,13 +1,12 @@
 require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
 
-describe "Fizz" do
+describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = Fizz(3)
 
     expect(fizz_3).to eq("Fizz")
   end
-describe "Buzz" do
   it 'returns "Buzz" when the number is divisible by 5' do
     fizz_5 = Buzz(5)
 
